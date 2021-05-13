f4a8f46198c1e5d32745ac4319d31f2190cacaec
ShoufaChen <shoufachen@163.com>
2021-05-13 20:05:56 +0800

    multi-process download

---
+++ multiproc_download.sh
@@ -0,0 +1,9 @@
+mkdir train
+
+while read url;
+do
+    while [[ "$(pgrep -fc wget)" -gt 9 && $c -gt 10 ]]; do sleep 1; done; 
+    ((c++));  
+    echo "Launching $url ($c)"; 
+    wget $url -P train >/dev/null 2>&1 && echo "$c finsihed" & 
+done < k700_2020_train_path.txt
